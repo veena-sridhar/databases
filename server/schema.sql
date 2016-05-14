0a4e649d5926c673ca48db277c61cf2286e83b47
veena-sridhar <v.sridhar91@gmail.com>
2016-05-13 19:46:54 -0700

    Creates tables for users and messages

--- server/schema.sql
+++ server/schema.sql
@@ -1,17 +1,48 @@
+DROP DATABASE IF EXISTS `chat`;
+
 CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+DROP TABLE IF EXISTS `Users`;
+    
+CREATE TABLE `Users` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `Username` VARCHAR(255) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
 );
+
+
+DROP TABLE IF EXISTS `Messages`;
+    
+CREATE TABLE `Messages` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `Message` VARCHAR(255) NULL DEFAULT NULL,
+  `RoomName` VARCHAR(255) NULL DEFAULT NULL,
+  `id_Users` INTEGER NULL DEFAULT NULL,
+  `CreationTime` TIMESTAMP NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+ALTER TABLE `Messages` ADD FOREIGN KEY (id_Users) REFERENCES `Users` (`id`);
+
+-- ALTER TABLE `Users` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `Messages` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ---
+-- Test Data
+-- ---
+
+-- INSERT INTO `Users` (`id`,`Username`) VALUES
+-- ('','');
+-- INSERT INTO `Messages` (`id`,`Message`,`RoomName`,`id_Users`,`CreationTime`) VALUES
+-- ('','','','','');
 
 /* Create other tables and define schemas for them here! */
 
 
 
-
 /*  Execute this file from the command line by typing:
- *    mysql -u root < server/schema.sql
+ *    mysql -u root -p < server/schema.sql
  *  to create the database and the tables.*/
 
